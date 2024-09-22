9f0efefa4ecd2155c734347abdf1dd0c80d71dbd
abhi-kakadiya <abhikakadiya8477@gmail.com>
2024-09-22 22:07:20 +0530

    Created Header, Lane and Tasks componenets

---
+++ src/components/Task/Task.js
@@ -0,0 +1,27 @@
+// import "./Task.css";
+import styled from "styled-components";
+
+const Title = styled.h3`
+	h3 {
+		width: 100%;
+		margin: 0;
+	}
+`;
+
+const TaskWrapper = styled.div`
+	background: darkGray;
+	padding: 20px;
+	border-radius: 20px;
+	margin: 0% 5% 5% 5%;
+`;
+
+function Task({ id, title, body, onDragStart }) {
+	return (
+		<TaskWrapper draggable onDragStart={(e) => onDragStart(e, id)}>
+			<Title>{title}</Title>
+			<p>{body}</p>
+		</TaskWrapper>
+	);
+}
+
+export default Task;
